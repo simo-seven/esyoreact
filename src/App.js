1c74f2082ce9bb26192f4c523081fe53b00a3b11
simo-seven <88766651+simo-seven@users.noreply.github.com>
2024-05-01 13:10:33 +0200

    added css and images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./css/bootstrap.min.css";
+import "./css/my.css";
+import "./css/style.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home.js";
+import NotFound from "./NotFound.js";
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
