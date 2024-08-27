e4035e3eac9e85f1da7c1f1b95c119874bb3f6cb
simo_seven <88766651+simo-seven@users.noreply.github.com>
2024-08-27 22:50:52 +0200

    SEO (pt.3) - TEST (twitter and facebook meta tags)

--- src/components/auditions/Auditions.js
+++ src/components/auditions/Auditions.js
@@ -26,8 +26,39 @@ const Auditions = ({ formatDate }) => {
         <meta
           name="description"
           content="Audition for the European Spirit of Youth Orchestra. Join top musicians, perform in prestigious venues, and grow your talent in a vibrant musical community."
-          />
+        />
         <link rel="canonical" href="/auditions" />
+        {/* Facebook tags */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Auditions - European Spirit of Youth Orchestra"
+        />
+        <meta
+          property="og:description"
+          content="Audition for the European Spirit of Youth Orchestra. Join top musicians, perform in prestigious venues, and grow your talent in a vibrant musical community."
+        />
+        <meta property="og:url" content="https://esyo.eu/auditions" />
+        <meta
+          property="og:image"
+          content="https://esyo.eu/static/media/LogoSmall.180757e2ef4a35c3599e.png"
+        />
+        {/* End Facebook tags */}
+        {/* Twitter tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Auditions - European Spirit of Youth Orchestra"
+        />
+        <meta
+          name="twitter:description"
+          content="Audition for the European Spirit of Youth Orchestra. Join top musicians, perform in prestigious venues, and grow your talent in a vibrant musical community."
+        />
+        <meta
+          name="twitter:image"
+          content="https://esyo.eu/static/media/LogoSmall.180757e2ef4a35c3599e.png"
+        />{" "}
+        {/* End Twitter tags */}
       </Helmet>
 
       {today > deadlineDate ? (
@@ -39,7 +70,11 @@ const Auditions = ({ formatDate }) => {
       ) : today < startDateDate ? (
         <div className="container-xxl py-5">
           <div className="container py-5 px-lg-5">
-            <UpcomingAuditions startDate={startDate} deadline={deadline} formatDate={formatDate} />
+            <UpcomingAuditions
+              startDate={startDate}
+              deadline={deadline}
+              formatDate={formatDate}
+            />
           </div>
         </div>
       ) : (
