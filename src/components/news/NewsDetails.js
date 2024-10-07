51d1fcb15a4982bc5832db5b3a5792a217943224
simo_seven <88766651+simo-seven@users.noreply.github.com>
2024-10-07 22:42:03 +0200

    autumn tour news

--- src/components/news/NewsDetails.js
+++ src/components/news/NewsDetails.js
@@ -17,33 +17,21 @@ const NewsDetails = ({ formatDate, renderBody }) => {
         <link rel="canonical" href={`https://esyo.eu/news/${id}`} />
 
         <meta property="og:type" content="article" />
-        <meta
-          property="og:title"
-          content={news.title}
-        />
+        <meta property="og:title" content={news.title} />
         <meta
           property="og:description"
           content={news.body.split(" ").slice(0, 20).join(" ") + "..."}
         />
         <meta property="og:url" content={`https://esyo.eu/news/${id}`} />
-        <meta
-          property="og:image"
-          content={`https://esyo.eu/${news.img}`}
-        />
+        <meta property="og:image" content={`https://esyo.eu/${news.img}`} />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content={news.title}
-        />
+        <meta name="twitter:title" content={news.title} />
         <meta
           name="twitter:description"
           content={news.body.split(" ").slice(0, 20).join(" ") + "..."}
         />
-        <meta
-          name="twitter:image"
-          content={`https://esyo.eu/${news.img}`}
-        />
+        <meta name="twitter:image" content={`https://esyo.eu/${news.img}`} />
       </Helmet>
 
       <div className="container-xxl py-5">
@@ -54,8 +42,24 @@ const NewsDetails = ({ formatDate, renderBody }) => {
               <div className="mt-10 mb-3 text-justify">
                 {renderBody(news.body)}
               </div>
+              <div className="mt-10 mb-3 text-justify">
+                {news.partnerSponsor.map((partner) => (
+                  <div key={partner.id} className="mt-10 mb-3 text-justify">
+                    <h2 className="text-white">{partner.mainOrTitle}</h2>
+                    {renderBody(partner.relatedOrganisationsInstitutions)}
+                  </div>
+                ))}
+              </div>
               <p className="mt-10">
-                <small>Published on: {formatDate(news.publishedDate)}</small>
+                <small>
+                  {news.author && (
+                    <>
+                      {news.author}
+                      <br />
+                    </>
+                  )}
+                  Published on {formatDate(news.publishedDate)}
+                </small>
               </p>
             </article>
           </div>
