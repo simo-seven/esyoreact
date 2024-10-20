4fa15b750651e7ec2caaeb7ba8cdc8286f08d0a3
simo_seven <88766651+simo-seven@users.noreply.github.com>
2024-10-20 21:31:20 +0200

    testing private events tag

--- src/components/tours/DesktopConcerts.js
+++ src/components/tours/DesktopConcerts.js
@@ -2,14 +2,14 @@ const DesktopConcerts = ({ concerts, bcg }) => {
   return (
     <div className={`container-xxl py-5 hideOnMobile ${bcg}`}>
       <div className="container py-5 px-lg-5">
-        <div className="container">
-          <div className="row">
-            {concerts.map((concert) => (
-              <div className="col-12" key={concert.id}>
-                <div className="card mb-3 nobordertransparent">
+        <div className="row">
+          {concerts.map((concert) => (
+            <div className="col-12" key={concert.id}>
+              <div className="card mb-3 nobordertransparent">
+                <div className="row g-0">
                   {concert.id % 2 === 0 ? (
                     // Output if the concert.id is even (aligned to the right)
-                    <div className="row g-0">
+                    <>
                       <div className="col-md-6 nobkg">
                         <div className="card-body text-end nobkg">
                           <h1 className="card-title text-uppercase text-white fw-bold">
@@ -18,27 +18,30 @@ const DesktopConcerts = ({ concerts, bcg }) => {
                           <h1 className="card-text text-white">
                             <span className="concertDate">{concert.date}</span>
                           </h1>
-                          <p className="card-text text-white">
-                            {concert.venue}
-                          </p>
+                          <p className="card-text text-white m-1">{concert.venue}</p>
                           <p className="card-text">
-                            <i className="fa-solid fa-clock"></i> {concert.time}
+                            {concert.time} <i className="fa-solid fa-clock"></i>
                           </p>
+                          {concert.private && (
+                            <p className="card-text">
+                              Private event <i className="fa-solid fa-lock"></i>
+                            </p>
+                          )}
                         </div>
                       </div>
-                      <div className="col-md-6 concertCardDetailsRight">
+                      <div className="col-md-6 concertCardDetailsRight d-flex align-items-center justify-content-center">
                         <div className="card-body">
-                          <p className="card-text">{concert.description}</p>
+                          <p className="card-text mb-3">{concert.description}</p>
                           <p className="card-text">{concert.program}</p>
                         </div>
                       </div>
-                    </div>
+                    </>
                   ) : (
                     // Output if the concert.id is odd (aligned to the left)
-                    <div className="row g-0">
-                      <div className="col-md-6 concertCardDetailsLeft">
+                    <>
+                      <div className="col-md-6 concertCardDetailsLeft d-flex align-items-center justify-content-center">
                         <div className="card-body">
-                          <p className="card-text">{concert.description}</p>
+                          <p className="card-text mb-3">{concert.description}</p>
                           <p className="card-text">{concert.program}</p>
                         </div>
                       </div>
@@ -50,20 +53,23 @@ const DesktopConcerts = ({ concerts, bcg }) => {
                           <h1 className="card-text text-white">
                             <span className="concertDate">{concert.date}</span>
                           </h1>
-                          <p className="card-text text-white">
-                            {concert.venue}
-                          </p>
+                          <p className="card-text text-white m-1">{concert.venue}</p>
                           <p className="card-text">
                             <i className="fa-solid fa-clock"></i> {concert.time}
                           </p>
+                          {concert.private && (
+                            <p className="card-text">
+                              <i className="fa-solid fa-lock"></i> Private event
+                            </p>
+                          )}
                         </div>
                       </div>
-                    </div>
+                    </>
                   )}
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
